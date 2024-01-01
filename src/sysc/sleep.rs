5b555c39f2c3c5312f03d117a8314bb7ca0337bc
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-01-01 10:00:16 +0100

    refactor

--- src/sysc/sleep.rs
+++ src/sysc/sleep.rs
@@ -1,10 +1,10 @@
 use esp_idf_svc::sys::esp_deep_sleep;
 use std::time::Duration;
 
-const INFINITE_SLEEP_TIME: u64 = 2_629_746_000_000; /* 1 month */
+const INFINITE_SLEEP_TIME: Duration = Duration::from_micros(2_629_746_000_000); /* 1 month */
 
 pub fn deep_sleep(time: Option<Duration>) {
     unsafe {
-        esp_deep_sleep(time.map_or(INFINITE_SLEEP_TIME, |time| time.as_micros() as u64));
+        esp_deep_sleep(time.unwrap_or(INFINITE_SLEEP_TIME).as_micros() as u64);
     }
 }
