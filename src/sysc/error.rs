f72bd87a4de969e012a44cbee03ddd61ea5474f3
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-03-13 22:29:25 +0100

    document errors

--- src/sysc/error.rs
+++ src/sysc/error.rs
@@ -5,26 +5,47 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum OsError {
+    /// Failed to connect to WiFi AP.
     #[error("wifi connect: {0}")]
     WifiConnect(EspError),
+
+    /// No usable AP found.
     #[error("offline")]
     NoInternet,
+
+    /// Error during connecting to or processing data from the PWMP server.
     #[error("pwmp: {0}")]
     PwmpError(#[from] pwmp_client::error::Error),
+
+    /// No environment sensor detected.
     #[error("environment sensor")]
     NoEnvSensor,
+
+    /// Generic ESP-IDF error.
     #[error("esp api: {0}")]
     Esp(#[from] EspError),
+
+    /// Specified ESSID was too long. (>=32 chars)
     #[error("SSID too long")]
     SsidTooLong,
+
+    /// Specified PSK was too long.
     #[error("PSK too long")]
     PskTooLong,
+
+    /// Partition metadata contains an invalid version string.
     #[error("Unexpected version format")]
     IllegalFirmwareVersion,
+
+    /// Failiure during conversions to/from [`Decimal`](pwmp_client::pwmp_msg::Decimal).
     #[error("Conversion beteen float/int and Decimal failed")]
     DecimalConversion,
+
+    /// Invalid battery voltage result.
     #[error("Invalid battery voltage")]
     IllegalBatteryVoltage,
+
+    /// Invalid partition metadata.
     #[error("Invalid/unexpected partition metadata")]
     IllegalPartitionMeta,
 }
