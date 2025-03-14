8a9ec1a6bb730c59618ece4681c7df9354144835
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-03-14 19:48:15 +0100

    add documentation for the brownout management driver

--- src/sysc/brownout.rs
+++ src/sysc/brownout.rs
@@ -1,6 +1,11 @@
+//! Brownout management driver.
+//!
+//! Currently this module only allows disabling the brownout protection feature.
+
 use esp_idf_svc::sys::RTC_CNTL_BROWN_OUT_REG;
 use std::ptr;
 
+/// Disable the brownout detector by zeroing the brownout control register ([`RTC_CNTL_BROWN_OUT_REG`](RTC_CNTL_BROWN_OUT_REG).)
 pub fn disable_brownout_detector() {
     unsafe {
         ptr::write_volatile(RTC_CNTL_BROWN_OUT_REG as *mut i32, 0);
