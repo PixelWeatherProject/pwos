3e847d78cae4b52b0d39216f7ce76b400babec3d
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-03-14 19:45:42 +0100

    add documentation for the battery driver

--- src/sysc/battery.rs
+++ src/sysc/battery.rs
@@ -1,3 +1,5 @@
+//! A driver for reading the battery supply voltage using the node's ADC.
+
 use super::{OsError, OsResult};
 use crate::{os_debug, os_error};
 use esp_idf_svc::{
@@ -17,29 +19,43 @@ use esp_idf_svc::{
 use pwmp_client::pwmp_msg::{dec, Decimal};
 use std::{rc::Rc, thread::sleep, time::Duration};
 
+/// GPIO pin where the output of the voltage divider is connected
 type BatteryGpio = Gpio2;
+/// The ADC hardware
 type BatteryAdc = ADC1;
+/// Alias for the ADC driver
 type BatteryAdcDriver = AdcDriver<'static, BatteryAdc>;
+/// Alias for the ADC channel driver
 type BatteryAdcChannelDriver = AdcChannelDriver<'static, BatteryGpio, Rc<BatteryAdcDriver>>;
 
+/// Input signal attenuation level
 const ATTEN: adc_atten_t = DB_11;
+/// Value of the first resistor of the voltage divider
 const R1: Decimal = dec!(20_000); // 20kOhm
+/// Value of the second resistor of the voltage divider
 const R2: Decimal = dec!(6_800); // 6.8kOhm
+/// ADC channel configuration
 const CONFIG: AdcChannelConfig = AdcChannelConfig {
-    attenuation: ATTEN,
-    calibration: Calibration::Curve,
-    resolution: Resolution::Resolution12Bit,
+    attenuation: ATTEN,              /* refer to the attenuation value above  */
+    calibration: Calibration::Curve, /* ADC auto-calibration type */
+    resolution: Resolution::Resolution12Bit, /* ADC resolution */
 };
+/// Maximum voltage we expect
 const MAX_VOLTAGE: Decimal = dec!(5.00);
-
+/// Critical voltage value that's still higher than the minimum supply voltage for the ESP32
 pub const CRITICAL_VOLTAGE: Decimal = dec!(2.70);
 
+/// Battery voltage measurement driver.
 pub struct Battery {
+    /// ADC driver handle
     adc: Rc<BatteryAdcDriver>,
+
+    /// ADC channel driver handle
     ch: BatteryAdcChannelDriver,
 }
 
 impl Battery {
+    /// Initiliaze a new instance of this driver using the given peripheral handles.
     pub fn new(adc: BatteryAdc, gpio: BatteryGpio) -> OsResult<Self> {
         let adc = Rc::new(BatteryAdcDriver::new(adc)?);
         let ch = BatteryAdcChannelDriver::new(Rc::clone(&adc), gpio, &CONFIG)?;
@@ -47,6 +63,7 @@ impl Battery {
         Ok(Self { adc, ch })
     }
 
+    /// Read the ADC value and calculate the voltage.
     pub fn read(&mut self, samples: u8) -> OsResult<Decimal> {
         let raw = self.read_raw(samples)?;
         let volts = Decimal::from(self.adc.raw_to_mv(&self.ch, raw)?) / dec!(1000);
@@ -69,6 +86,7 @@ impl Battery {
         Ok(result.trunc_with_scale(2))
     }
 
+    /// Read the raw ADC value.
     fn read_raw(&mut self, samples: u8) -> OsResult<u16> {
         let mut avg = dec!(0);
 
