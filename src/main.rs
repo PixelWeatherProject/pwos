abdcb11a9aa4fefd1d28aa3b112b35f2ffa5563c
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2026-05-28 13:09:43 +0200

    update internal temperature sensor initialization to use config struct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use crate::sysc::{logging::OsLogger, ReportableError};
 use esp_idf_svc::hal::{
     i2c::{config::Config, I2cDriver},
-    temp_sensor::TempSensorDriver,
+    temp_sensor::{config::Config as TempSensorConfig, TempSensorDriver},
     units::FromValueType,
 };
 use std::time::Instant;
@@ -94,8 +94,9 @@ fn main() {
     .expect("Failed to initialize I2C");
 
     log::debug!("Initializing internal temperature sensor");
-    let mut temp_sensor = TempSensorDriver::new(&Default::default(), peripherals.temp_sensor)
-        .expect("Failed to initialize internal temperature sensor driver");
+    let mut temp_sensor =
+        TempSensorDriver::new(&TempSensorConfig::default(), peripherals.temp_sensor)
+            .expect("Failed to initialize internal temperature sensor driver");
     temp_sensor
         .enable()
         .expect("Failed to enable internal temperature sensor driver");
@@ -111,7 +112,7 @@ fn main() {
         i2c,
         peripherals.wifi.modem,
         peripherals.wifi.sys_loop,
-        temp_sensor,
+        &temp_sensor,
         led,
         &nvs,
         &mut ota,
